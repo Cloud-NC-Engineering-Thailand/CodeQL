04174c697624a19fb223f88c7a1276d1db31bff8
Sirinat Paphatsirinatthi <cpfapp@gmail.com>
2023-11-28 18:27:30 +0700

    Create pep8break.py

---
+++ python-code-folder/pep8break.py
@@ -0,0 +1,8 @@
+foo = long_function_name(var_one, var_two,
+    var_three, var_four)
+
+# Further indentation required as indentation is not distinguishable.
+def long_function_name(
+    var_one, var_two, var_three,
+    var_four):
+    print(var_one)
